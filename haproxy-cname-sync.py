e225418a6c1b10138f1e19adddb28dbc9ac86df1
James Fryman <james@fryman.io>
2016-10-07 12:22:01 -0500

    savegame 3

--- haproxy-cname-sync.py
+++ haproxy-cname-sync.py
@@ -15,14 +15,22 @@
 import requests
 import os
 import re
+import time
 import pprint
 
 # First Hammer
-CNAME_MAP_FILE = os.getenv('A0_CNAME_MAP_FILE', "./test/maps/domain-to-tenant.map")
-REGION_MAP_FILE = os.getenv('A0_CNAME_REGION_MAP_FILE', "./test/maps/domain-to-backend.map")
-ENDPOINT = os.getenv('A0_CNAME_ENDPOINT', 'http://localhost:8080/db')
-SSL_DIR = os.getenv('A0_CNAME_SSL_DIR', './test/ssl')
-TOKEN = os.getenv('A0_CNAME_TOKEN', 'deadbeef')
+CNAME_MAP_FILE = os.getenv('CNAMER_MAP_FILE')
+REGION_MAP_FILE = os.getenv('CNAMER_REGION_MAP_FILE')
+ENDPOINT = os.getenv('CNAMER_ENDPOINT')
+SSL_DIR = os.getenv('CNAMER_SSL_DIR')
+TOKEN = os.getenv('CNAMER_TOKEN')
+MAX_HTTP_RETRIES=10
+ENV = os.getenv('ENV', 'dev')
+
+session = requests.Session()
+adapter = requests.adapters.HTTPAdapter(max_retries=MAX_HTTP_RETRIES)
+session.mount('http://', adapter)
+session.mount('https://', adapter)
 
 def write_file(target, contents):
     file = open(target, "w")
@@ -31,8 +39,10 @@ def write_file(target, contents):
 
 def write_haproxy_map(mappings, target):
     file = open(target, "w")
-    for key, value in mappings:
-        file.write("{} {}").format(key, walue)
+    for mapping in mappings:
+        for key, value in mapping.items():
+            entry = " ".join([key, value]) + '\n'
+            file.write(entry)
     file.close()
 
 def eu_region_regex():
@@ -45,31 +55,45 @@ def extract_region(mapping):
     backend = mapping['backend']
 
     if re.match(eu_region_regex(), backend) is not None:
-        return { mapping['backend']: 'eu'}
+        return { mapping['backend']: 'eu-{}'.format(ENV) }
     elif re.match(au_region_regex(), backend) is not None:
-        return { mapping['backend']: 'au'}
+        return { mapping['backend']: 'au-{}'.format(ENV) }
     else:
-        return { mapping['backend']: 'us'}
+        return { mapping['backend']: 'us-{}'.format(ENV) }
 
 def extract_cname(mapping):
    return {mapping['cname']: mapping['backend']}
 
-def get_cname_map(endpoint):
-    return requests.get(endpoint).json()
+def get_endpoint(endpoint, max_tries=MAX_HTTP_RETRIES):
+    headers = {"Authorization": "Bearer {}".format(TOKEN)}
+
+    remaining_tries = max_tries
+    while remaining_tries > 0:
+        try:
+            return session.get(endpoint, headers=headers).json()
+        except:
+            time.sleep(1)
+
+        remaining_tries = remaining_tries - 1
+
+    raise Exception("Unable to get data")
 
 def save_certificate(mapping, ssl_dir):
     cert, key = (mapping['cert'], mapping['key'])
-    ssl_file = "{}/{}.pem".format(ssl_dir, mapping['cname'])
-    ssl_file_content = "{}{}".format(cert, key)
+    filename = mapping['cname'] + ".pem"
+
+    ssl_file = "/".join([ssl_dir, filename])
+    ssl_file_content = "".join([cert, key])
     write_file(ssl_file, ssl_file_content)
 
 def main():
-    mappings = get_cname_map(ENDPOINT)
+    mappings = get_endpoint(ENDPOINT)
 
-    cname_map = map(extract_cname, mappings)
-    region_map = map(extract_region, mappings)
-    #write_haproxy_map(cname_map, CNAME_MAP_FILE)
-    #write_haproxy_map(region_map, REGION_MAP_FILE)
+    cname_map = [extract_cname(m) for m in mappings]
+    region_map = [extract_region(m) for m in mappings]
+
+    write_haproxy_map(cname_map, CNAME_MAP_FILE)
+    write_haproxy_map(region_map, REGION_MAP_FILE)
 
     for mapping in mappings:
         save_certificate(mapping, SSL_DIR)
